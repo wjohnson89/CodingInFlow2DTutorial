33e050e5e917f86334b7f59f5c408cbe4f560e3d
Wayne <waj@bu.edu>
2023-12-31 09:38:55 -0500

    First commit. Built up to adding item count.

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+
+    [SerializeField] private TextMeshProUGUI cherriesText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            // Remove item
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "Cherries: " + cherries;
+        }
+    }
+}
